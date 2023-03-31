4b0358fa70b7f659eb85d0a3ed6af3a11e5438b8
iacobucci <iacobuccivalerio@gmail.com>
2023-04-01 00:45:42 +0200

    update to version version = "0.0.7"

--- src/vml_parser/vml.py
+++ src/vml_parser/vml.py
@@ -1,6 +1,7 @@
 #!/bin/python3
 import sys
 import os
+import json
 
 class Element:
     def __init__(self, name):
@@ -18,6 +19,10 @@ class Element:
 
     def append(self, child):
         self.children.append(child)
+        
+    def setchecked(self, checked):
+        self.hascheckbox = True
+        self.ischecked = checked
 
     def __str__(self):
         if self.hascheckbox:
@@ -35,7 +40,7 @@ class Element:
         return str(self)
 
 
-def parse(lines : list[str]) -> list[Element]:
+def parse(lines: list[str]) -> list[Element]:
     root = []
     for line in lines:
         if line.startswith("\n"):
@@ -59,53 +64,115 @@ def parse(lines : list[str]) -> list[Element]:
 
     return root
 
-def dump(obj : object, level: int = 0) -> list[str]:
+def dump(obj: list[Element], level: int = 0) -> list[str]:
     lines = []
-    
+
     for element in obj:
         # check if element has children
         if len(element.children) == 0:
             # check if element has checkbox
             if element.hascheckbox:
                 if element.ischecked:
-                    lines.append("\t" * level + "[x] " + element.name + "\n")
+                    lines.append("\t" * level + "[x] " + element.name)
                 else:
-                    lines.append("\t" * level + "[ ] " + element.name + "\n")
+                    lines.append("\t" * level + "[ ] " + element.name)
             else:
-                lines.append("\t" * level + element.name + "\n")
+                lines.append("\t" * level + element.name)
         else:
             # check if element has checkbox
             if element.hascheckbox:
                 if element.ischecked:
-                    lines.append("\t" * level + "[x] " + element.name + "\n")
+                    lines.append("\t" * level + "[x] " + element.name)
                 else:
-                    lines.append("\t" * level + "[ ] " + element.name + "\n")
+                    lines.append("\t" * level + "[ ] " + element.name)
             else:
-                lines.append("\t" * level + element.name + "\n")
-                
+                lines.append("\t" * level + element.name)
+
             # recursively call the function for children elements
-            lines.extend(dump(element.children, level = level + 1))
+            lines += dump(element.children, level + 1)  
+
+    return lines
+
+def dumps(lines : list[str]) -> list[str]:
+    def convert(obj) -> list[Element]:
+        root = []
+
+        if not isinstance(obj, list):
+            obj = [obj]
         
-    return "".join(lines)
-    
+        for item in obj:
+            if isinstance(item, str):
+                element = Element(item)
+            if isinstance(item, dict):
+                name = list(item.keys())[0]
+                element = Element(name)
+                children = item[name]
+                element.children = convert(children)
+                if "checked" in item.keys():
+                    element.hascheckbox = True
+                    element.ischecked = item["checked"]
+            root.append(element)
+        return root
+
+    root = convert(json.loads("".join(lines)))
+    data = dump(root)
+    return data
+                
+
+def usage():
+    print("Usage: vml [-d] <file.vml> [file2.vml] [file3.vml] ...")
+
 
 def main():
-    # check if this script is piped to stdin
-    if not sys.stdin.isatty():
-        # read from stdin
-        lines = sys.stdin.readlines()
-        # parse lines
-        print(parse(lines))
-        # exit
-        sys.exit(0)
+    # check for arguments
+    if len(sys.argv) == 1 and sys.stdin.isatty():
+        usage()
+        sys.exit(1)
+    
+    if len(sys.argv) > 1 and sys.stdin.isatty():
+        if sys.argv[1] == "-h" or sys.argv[1] == "--help":
+            usage()
+            sys.exit(0)
 
-    # for every file passed as an argument
-    for filename in sys.argv[1:]:
-        # check if filename is a regular file
-        if os.path.isfile(filename):
-            with open(filename, "r") as f:
-                lines = f.readlines()
+    # get dashed arguments
+    dashed_args = []
+    for arg in sys.argv[1::]:
+        if arg.startswith("-"):
+            dashed_args.append(arg)
+        else:
+            break
+        
+    # dump logic
+    if "-d" in dashed_args or "--dump" in dashed_args:
+        if not sys.stdin.isatty():
+            lines = sys.stdin.readlines()
+
+            for line in dumps(lines):
+                print(line)
+
+            sys.exit(0)
+
+        for filename in sys.argv[1:]:
+            if os.path.isfile(filename):
+                with open(filename, "r") as f:
+                    lines = f.readlines()
+
+                for line in dumps(lines):
+                    print(line)
+        sys.exit(0)
+        
+    #parse logic
+    else:
+        if not sys.stdin.isatty():
+            lines = sys.stdin.readlines()
             print(parse(lines))
+            sys.exit(0)
+
+        for filename in sys.argv:
+            if os.path.isfile(filename):
+                with open(filename, "r") as f:
+                    lines = f.readlines()
+                print(parse(lines))
 
 if __name__ == "__main__":
     main()
